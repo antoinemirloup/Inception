c1e7a1186c94a10c423a255ab8855a64789bf4f9
Antoine Mirloup <antoine.mirloup@gmail.com>
2024-10-08 15:20:42 +0200

    amirloup 2024-10-08 15:20:42

--- srcs/requirements/mariadb/conf/create_database.sh
+++ srcs/requirements/mariadb/conf/create_database.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-SQL_DATABASE="my_database"
-SQL_USER="my_user"
-SQL_PASSWORD="my_password"
-SQL_ROOT_PASSWORD="my_root_password"
-
 /etc/init.d/mariadb start
 # service mysql start;
+
+until mysqladmin ping --silent; do
+    echo "Attente de MariaDB..."
+    sleep 5
+done
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysqladmin -u root -p"$SQL_ROOT_PASSWORD" shutdown
 exec mysqld_safe
